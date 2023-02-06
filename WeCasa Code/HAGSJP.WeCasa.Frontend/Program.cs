358682f004b8109e591197686d32cf8e364f9a3b
githelsui <githelsuico@gmail.com>
2023-02-05 18:31:11 -0800

    [Frontend] Setup Reactjs ASP.NET Core app

---
+++ WeCasa Code/HAGSJP.WeCasa.Frontend/Program.cs
@@ -0,0 +1,33 @@
+﻿var builder = WebApplication.CreateBuilder(args);
+
+// Add services to the container.
+
+builder.Services.AddControllersWithViews();
+
+var app = builder.Build();
+
+// Configure the HTTP request pipeline.
+if (app.Environment.IsDevelopment())
+{
+    app.UseSwagger();
+    app.UseSwaggerUI();
+}
+else
+{
+    app.UseDefaultFiles();
+    app.UseStaticFiles();
+}
+
+app.UseHttpsRedirection();
+app.UseStaticFiles();
+app.UseRouting();
+
+
+app.MapControllerRoute(
+    name: "default",
+    pattern: "{controller}/{action=Index}/{id?}");
+
+app.MapFallbackToFile("index.html");
+
+app.Run();
+
