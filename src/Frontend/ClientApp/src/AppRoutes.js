f5c1e947236d84238d7543ef0e61be85e03fbff0
githelsui <githelsuico@gmail.com>
2023-02-08 16:12:08 -0800

    [Frontend] Registration Page elements

--- src/Frontend/ClientApp/src/AppRoutes.js
+++ src/Frontend/ClientApp/src/AppRoutes.js
@@ -1,11 +1,12 @@
 import { Counter } from "./components/Counter";
 import { FetchData } from "./components/FetchData";
 import { Home } from "./components/Home";
+import { Registration } from "./components/Registration";
 
 const AppRoutes = [
   {
     index: true,
-    element: <Home />
+    element: <Registration />
   },
   {
     path: '/counter',
