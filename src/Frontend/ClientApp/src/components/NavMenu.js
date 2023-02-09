b6e756b822efc779a185331bce76977b65a5b4de
githelsui <githelsuico@gmail.com>
2023-02-08 16:18:16 -0800

    [Frontend] Organize frontend files

--- src/Frontend/ClientApp/src/components/NavMenu.js
+++ src/Frontend/ClientApp/src/components/NavMenu.js
@@ -1,7 +1,7 @@
 import React, { Component } from 'react';
 import { Collapse, Navbar, NavbarBrand, NavbarToggler, NavItem, NavLink } from 'reactstrap';
 import { Link } from 'react-router-dom';
-import './NavMenu.css';
+import '../style/NavMenu.css';
 
 export class NavMenu extends Component {
   static displayName = NavMenu.name;
@@ -25,7 +25,7 @@ export class NavMenu extends Component {
     return (
       <header>
         <Navbar className="navbar-expand-sm navbar-toggleable-sm ng-white border-bottom box-shadow mb-3" container light>
-          <NavbarBrand tag={Link} to="/">HAGSJP.WeCasa.Frontend</NavbarBrand>
+          <NavbarBrand tag={Link} to="/">WeCasa</NavbarBrand>
           <NavbarToggler onClick={this.toggleNavbar} className="mr-2" />
           <Collapse className="d-sm-inline-flex flex-sm-row-reverse" isOpen={!this.state.collapsed} navbar>
             <ul className="navbar-nav flex-grow">
