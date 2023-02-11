bb980a4f100845f144be7ba28176b31862856bd8
githelsui <githelsuico@gmail.com>
2023-02-11 15:02:44 -0800

    [Frontend/Registration] Successful and failure account creation user flows

--- src/Frontend/HAGSJP.WeCasa.Frontend/ClientApp/src/components/Registration.js
+++ src/Frontend/HAGSJP.WeCasa.Frontend/ClientApp/src/components/Registration.js
@@ -1,103 +1,125 @@
-﻿import React, { Component } from 'react';
-import { Form, Input, Button } from 'antd';
+﻿import React, { Component, useState, useEffect } from 'react';
+import { Form, Input, Button, notification } from 'antd';
 import axios from 'axios';
-
-const submitForm = (values) => {
-    var proceedRegistration = true;
-
-    let userAccount = {
-        FirstName: values.firstName,
-        LastName: values.lastName,
-        Username: values.email,
-        Password: values.password1
-    };
-
-    // -- Client-side Input Validation
-
-    // Blank User Inputs
-    for (let key in userAccount) {
-        if (userAccount[key] == null) {
-            proceedRegistration = false;
-            break;
-        }
-    }
-
-    // Password Matching Validation
-    proceedRegistration = values.password1 == values.password2
-
-    if (proceedRegistration) {
-        registerAccount(userAccount)
-    } else {
-        userFailureRegistrationView();
-    }
-};
-
-const registerAccount = (userAccount) => {
-    axios.post('registration', userAccount)
-        .then(res => {
-            console.log(res.data)
-            var isSuccessful = res.data['isSuccessful'];
-            var errorStatus = res.data['errorStatus'];
-            if (isSuccessful) {
-                successRegistrationView();
-            } else if (errorStatus != 0) {
-                errorRegistrationView();
-            } else {
-                userFailureRegistrationView();
-            }
-        })
-        .catch((error) => { console.error(error) });
-};
-
-const userFailureRegistrationView = () => {
-    // Display all input validation messages
-    console.log("User Failure Cases")
-}
-
-const successRegistrationView = () => {
-    // Transition to next view AccountCreationSuccess
-    console.log("Success")
-}
-
-const errorRegistrationView = () => {
-    // Display system error messages
-    console.log("System Error")
-}
+import '../styles/Registration.css';
 
 export class Registration extends Component {
     static displayName = Registration.name;
 
     constructor(props) {
         super(props);
-        this.state = { registrationResult: [], loading: true };
+        this.state = {
+            registrationResults: false,
+            inputFailures: false,
+            loading: true,
+            newAccount: null
+        };
+    }
+
+    submitForm = (values) => {
+        var failureMessage = '';
+
+        let userAccount = {
+            FirstName: values.firstName,
+            LastName: values.lastName,
+            Username: values.email,
+            Password: values.password1
+        };
+
+        // -- Client-side Input Validation
+
+        // Blank User Inputs
+        for (let key in userAccount) {
+            if (userAccount[key] == null) {
+                this.state.inputFailures = true;
+                failureMessage ='Empty fields are not accepted.'
+                break;
+            }
+        }
+
+        // Password Matching Validation
+        if (values.password1 != values.password2) {
+            this.state.inputFailures = true;
+            failureMessage += 'Passwords must match.'
+        }
+
+        if (!this.state.inputFailures) {
+            this.registerAccount(userAccount)
+        } else {
+            this.failureRegistrationView(failureMessage);
+        }
+    };
+
+    registerAccount = (userAccount) => {
+        if (!this.state.inputFailures) {
+            axios.post('registration', userAccount)
+                .then(res => {
+                    console.log(res.data)
+                    var isSuccessful = res.data['isSuccessful'];
+                    if (isSuccessful) {
+                        this.successRegistrationView(userAccount['Username']);
+                    } else {
+                        this.failureRegistrationView(res.data['message']);
+                    }
+                })
+                .catch((error) => { console.error(error) });
+        }
+    };
+
+    failureRegistrationView = (failureMessage) => {
+        // Accounts for user failure cases and system errors
+        this.state.registrationResults = false
+        console.log("registration result: " + this.state.registrationResults)
+        console.log("User Failure Cases")
+        notification.open({
+            message: "Try again.",
+            description: failureMessage,
+            duration: 10,
+            placement: "topRight",
+        });
+    }
+
+    successRegistrationView = (accountName) => {
+        this.state.registrationResults = true
+        this.state.newAccount = accountName
+        this.forceUpdate();
     }
 
     render() {
         return (
             <div>
                 <h1>Register Account</h1>
-                <Form id="registrationForm" onFinish={(values) => submitForm(values)}>
-                    <Form.Item name="firstName">
-                        <Input placeholder="First Name" />
-                    </Form.Item>
+                {
+                    this.state.registrationResults ?
+                        (<div><h2>Account {this.state.newAccount} created successfully.</h2></div>)
+                        :
+                        (<div id="RegistrationForm">
+                            <Form id="registrationForm" onFinish={(values) => this.submitForm(values)}>
+                                <Form.Item name="firstName">
+                                    <Input placeholder="First Name" />
+                                </Form.Item>
 
-                    <Form.Item name="lastName">
-                        <Input placeholder="Last Name" />
-                    </Form.Item>
-                    <Form.Item name="email">
-                        <Input placeholder="Email" />
-                    </Form.Item>
+                                <Form.Item name="lastName">
+                                    <Input placeholder="Last Name" />
+                                </Form.Item>
+                                <Form.Item name="email">
+                                    <Input placeholder="Email" />
+                                </Form.Item>
 
-                    <Form.Item name="password1">
-                        <Input.Password placeholder="Password" />
-                    </Form.Item>
+                                <Form.Item name="password1">
+                                    <Input.Password placeholder="Password" />
+                                </Form.Item>
 
-                    <Form.Item name="password2">
-                        <Input.Password placeholder="Reenter Password" />
-                    </Form.Item>
-                    <Button htmlType="submit">Register</Button>
-                </Form>
-
+                                <Form.Item name="password2">
+                                    <Input.Password placeholder="Reenter Password" />
+                                </Form.Item>
+                                <Button type="primary" htmlType="submit">Register</Button>
+                            </Form>
+                        </div>)
+                }
+                <div id="LoginButton">
+                    <Button>Login</Button>
+                </div>
             </div>
         );
     }
