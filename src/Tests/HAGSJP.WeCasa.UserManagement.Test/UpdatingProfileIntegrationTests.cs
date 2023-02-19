430e9d783ff4f089cfa282b735a4df72152595c2
githelsui <githelsuico@gmail.com>
2023-02-18 17:41:58 -0800

    [Updating Profile] Integration Tests fixed

--- src/Tests/HAGSJP.WeCasa.UserManagement.Test/UpdatingProfileIntegrationTests.cs
+++ src/Tests/HAGSJP.WeCasa.UserManagement.Test/UpdatingProfileIntegrationTests.cs
@@ -19,11 +19,12 @@ namespace HAGSJP.WeCasa.UserManagement.Test
             var stopwatch = new Stopwatch();
             var expected = 5;
             var systemUnderTest = new UserManager();
+            UserAccount testUser = new UserAccount("oldFirst", "oldLast", "updatefirstname@gmail.com");
+            systemUnderTest.RegisterUser("oldFirst", "oldLast", "updatefirstname@gmail.com", "P@ssw0rd");
 
             // Act
             stopwatch.Start();
-            UserAccount testUser = new UserAccount("test@gmail.com");
-            var testResult = systemUnderTest.UpdateFirstName(testUser, "first");
+            var testResult = systemUnderTest.UpdateFirstName(testUser, "newFirst");
             stopwatch.Stop();
 
             // turn ms to seconds
@@ -43,11 +44,12 @@ namespace HAGSJP.WeCasa.UserManagement.Test
             var stopwatch = new Stopwatch();
             var expected = 5;
             var systemUnderTest = new UserManager();
+            UserAccount testUser = new UserAccount("oldFirst", "oldLast", "updatelastname@gmail.com");
+            systemUnderTest.RegisterUser("oldFirst", "oldLast", "updatelastname@gmail.com", "P@ssw0rd");
 
             // Act
             stopwatch.Start();
-            UserAccount testUser = new UserAccount("test@gmail.com");
-            var testResult = systemUnderTest.UpdateLastName(testUser, "last");
+            var testResult = systemUnderTest.UpdateFirstName(testUser, "newLast");
             stopwatch.Stop();
 
             // turn ms to seconds
@@ -67,10 +69,12 @@ namespace HAGSJP.WeCasa.UserManagement.Test
             var stopwatch = new Stopwatch();
             var expected = 5;
             var systemUnderTest = new UserManager();
+            UserAccount testUser = new UserAccount("oldFirst", "oldLast", "updateusername@gmail.com");
+            systemUnderTest.RegisterUser("oldFirst", "oldLast", "updateusername@gmail.com", "P@ssw0rd");
+
 
             // Act
             stopwatch.Start();
-            UserAccount testUser = new UserAccount("test@gmail.com");
             var testResult = systemUnderTest.UpdateUsername(testUser, "new@gmail.com");
             stopwatch.Stop();
 
@@ -92,11 +96,13 @@ namespace HAGSJP.WeCasa.UserManagement.Test
             var expected = 5;
             var systemUnderTest = new UserManager();
             HashSaltSecurity hashSaltSecurity = new HashSaltSecurity();
+            UserAccount testUser = new UserAccount("oldFirst", "oldLast", "updatepassword@gmail.com");
+            systemUnderTest.RegisterUser("oldFirst", "oldLast", "updatepassword@gmail.com", "P@ssw0rd");
+
 
             // Act
             stopwatch.Start();
-            UserAccount testUser = new UserAccount("test@gmail.com");
-            var newPassword = "P@ssw0rd";
+            var newPassword = "P@@ssw0rd";
             string salt = BitConverter.ToString(hashSaltSecurity.GenerateSalt(newPassword));
             string encryptedPass = hashSaltSecurity.GetHashSaltCredentials(newPassword, salt);
             var testResult = systemUnderTest.UpdatePassword(testUser, salt, encryptedPass);
@@ -117,24 +123,26 @@ namespace HAGSJP.WeCasa.UserManagement.Test
         public void ShouldPerformBulkOps60Sec()
         {
             // Arrange
+            var systemUnderTest = new UserManager();
             var results = new ArrayList();
             var accounts = new ArrayList();
-            for (int i = 0; i < 10000; i++)
+            for (int i = 0; i < 1000; i++)
             {
                 var uniqueEmail = "user" + i.ToString() + "@gmail.com";
-                accounts.Add(new UserAccount(uniqueEmail));
+                var tempUser = new UserAccount("oldFirst", "oldLast", uniqueEmail);
+                accounts.Add(tempUser);
+                systemUnderTest.RegisterUser("oldFirst", "oldLast", uniqueEmail, "P@ssw0rd");
             }
             var stopwatch = new Stopwatch();
             var expected = 60;
-            var systemUnderTest = new UserManager();
 
             // Act
             stopwatch.Start();
-            for (int i = 0; i < 10000; i++)
+            for (int i = 0; i < 1000; i++)
             {
                 UserAccount user = (UserAccount)accounts[i];
                 var newEmail = "new" + i.ToString() + "@gmail.com";
-                var res = systemUnderTest.UpdateFirstName(user, newEmail);
+                var res = systemUnderTest.UpdateUsername(user, newEmail);
                 results.Add(res);
             }
             stopwatch.Stop();
@@ -146,7 +154,7 @@ namespace HAGSJP.WeCasa.UserManagement.Test
             Assert.IsNotNull(actual);
             Assert.IsTrue(actual >= 0);
             Assert.IsTrue(actual <= expected);
-            for (int i = 0; i < 10000; i++)
+            for (int i = 0; i < 1000; i++)
             {
                 Result res = (Result)results[i];
                 Assert.IsTrue(res.IsSuccessful);
--- src/Tests/HAGSJP.WeCasa.UserManagement.Test/UpdatingProfileUnitTests.cs
+++ src/Tests/HAGSJP.WeCasa.UserManagement.Test/UpdatingProfileUnitTests.cs
@@ -4,6 +4,7 @@ namespace HAGSJP.WeCasa.UserManagement.Test
     [TestClass]
     public class UpdatingProfileUnitTests
     {
+        //Make sure there is no database connection
         [TestMethod]
         public void ShouldCreateInstanceWithParameterCtor()
         {
