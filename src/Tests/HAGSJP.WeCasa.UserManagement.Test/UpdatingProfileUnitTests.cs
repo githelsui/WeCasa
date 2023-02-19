2dc113b4bc49cafbbfb053ad17b387b0f3411875
githelsui <githelsuico@gmail.com>
2023-02-18 16:58:54 -0800

    [Updating Profile] Integration Tests

---
+++ src/Tests/HAGSJP.WeCasa.UserManagement.Test/UpdatingProfileUnitTests.cs
@@ -0,0 +1,13 @@
+﻿using System;
+namespace HAGSJP.WeCasa.UserManagement.Test
+{
+    [TestClass]
+    public class UpdatingProfileUnitTests
+    {
+        [TestMethod]
+        public void ShouldCreateInstanceWithParameterCtor()
+        {
+
+        }
+    }
+}
