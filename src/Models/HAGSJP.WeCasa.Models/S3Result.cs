4a53f94ee5492aa81841fd73e4f153142ef6e619
githelsui <githelsuico@gmail.com>
2023-03-09 20:43:23 -0800

    File View Modal

--- src/Models/HAGSJP.WeCasa.Models/S3Result.cs
+++ src/Models/HAGSJP.WeCasa.Models/S3Result.cs
@@ -3,9 +3,9 @@ namespace HAGSJP.WeCasa.Models
 {
     public class S3Result : Result
     {
-        public List<FileModel> Files;
+        public List<FileModel> Files { get; set; }
 
-        public Object? ReturnedObject;
+        public Object? ReturnedObject { get; set; }
 
         public S3Result() { }
 
