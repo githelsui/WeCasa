7f27573a6426237f708406f542c6c11e5d90809f
githelsui <githelsuico@gmail.com>
2023-04-05 15:27:30 -0700

    [Chores List] Backend Model + DAO Setup

---
+++ src/Data Access Layer/HAGSJP.WeCasa.sqlDataAccess/ChoresDAO.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using HAGSJP.WeCasa.Models;
+using MySqlConnector;
+
+namespace HAGSJP.WeCasa.sqlDataAccess
+{
+	public class ChoresDAO : AccountMariaDAO
+	{
+        private string _connectionString;
+        private DAOResult result;
+
+        public MySqlConnectionStringBuilder BuildConnectionString()
+        {
+            var builder = new MySqlConnectionStringBuilder
+            {
+                Server = "localhost",
+                Port = 3306,
+                UserID = "HAGSJP.WeCasa.SqlUser",
+                Password = "cecs491",
+                Database = "HAGSJP.WeCasa"
+            };
+            return builder;
+        }
+
+        public DAOResult CreateChore()
+		{
+            throw new NotImplementedException();
+		}
+
+        public DAOResult UpdateChore()
+        {
+            throw new NotImplementedException();
+        }
+
+        public DAOResult GetChore()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
+
---
+++ src/Models/HAGSJP.WeCasa.Models/Chore.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Text.Json.Serialization;
+
+namespace HAGSJP.WeCasa.Models
+{
+    [Serializable]
+    public class Chore
+	{
+        public int? ChoreId { get; set; }
+        public String Name { get; set; }
+        public DateTime? ResetTime { get; set; }
+        public String? Notes { get; set; }
+        public String? Assignment { get; set; }
+        public int GroupId { get; set; }
+        public Boolean? IsRepeated { get; set; }
+        public Boolean? IsCompleted { get; set; }
+
+        [JsonConstructor]
+        public Chore(){}
+	}
+}
+
