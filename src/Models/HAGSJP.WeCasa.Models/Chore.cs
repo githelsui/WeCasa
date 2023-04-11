c7b0c5fa8e0128b41a6e31aa0129f07575e5908a
githelsui <githelsuico@gmail.com>
2023-04-11 16:25:37 -0700

    Update Chore.cs

--- src/Models/HAGSJP.WeCasa.Models/Chore.cs
+++ src/Models/HAGSJP.WeCasa.Models/Chore.cs
@@ -10,9 +10,9 @@ namespace HAGSJP.WeCasa.Models
         public String Name { get; set; }
         public DateTime? ResetTime { get; set; } // Chore due date
         public String? Notes { get; set; }
-        public String? Assignment { get; set; }
+        public String? Assignment { get; set; } //TODO: Remove property in model 
         public int GroupId { get; set; }
-        public Boolean? IsRepeated { get; set; }
+        public Boolean? IsRepeated { get; set; } //TODO: Remove property in model
         public Boolean? IsCompleted { get; set; }
 
         public DateTime? Created { get; set; }
@@ -20,6 +20,7 @@ namespace HAGSJP.WeCasa.Models
         public DateTime? LastUpdated { get; set; }
         public String? LastUpdatedBy { get; set; }
         public List<string> AssignedTo { get; set; }
+        public String? Repeats { get; set; }
 
         [JsonConstructor]
         public Chore(){}
