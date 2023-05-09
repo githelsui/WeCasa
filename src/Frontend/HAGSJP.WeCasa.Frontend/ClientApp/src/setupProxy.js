6ca13b6ff6bcdc1427db64a0c2f0ddba6c7027a8
judyli840 <51252982+judyli840@users.noreply.github.com>
2023-05-09 02:30:51 -0700

    [AWS] add backend url port

--- src/Frontend/HAGSJP.WeCasa.Frontend/ClientApp/src/setupProxy.js
+++ src/Frontend/HAGSJP.WeCasa.Frontend/ClientApp/src/setupProxy.js
@@ -2,7 +2,7 @@ const { createProxyMiddleware } = require('http-proxy-middleware');
 const { env } = require('process');
 
 const target = env.ASPNETCORE_HTTPS_PORT ? `https://localhost:${env.ASPNETCORE_HTTPS_PORT}` :
-  env.ASPNETCORE_URLS ? env.ASPNETCORE_URLS.split(';')[0] : 'http://localhost:28608';
+  env.ASPNETCORE_URLS ? `${env.ASPNETCORE_URLS.split(';')[0]}:7131` : 'http://localhost:28608';
 
 const context = [
     "/weatherforecast",
